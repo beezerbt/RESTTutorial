a2567c7ad8d1b3b2881c335926cd4b0bdd299591
bart dart <kambiz.shahri@gmail.com>
2021-10-11 13:16:16 +0000

    Figured out the port issue and the routing.

--- index.js
+++ index.js
@@ -12,11 +12,26 @@
 var http = require('http')
   , https = require('https')
   , express = require('express')
-  , app = express();
+  , birds = require('./birds')
+  , app = express()
+app.use('/birds', birds)
+
+// app.use(express.json)
+// var subscriberRouter = require('./routes/subscribers')
+// app.use('/', subscriberRouter)
 http.createServer(app).listen(80);
-//   app.get('/', function (req, res) {
+// app.get('/', function (req, res) {
 //     res.header('Content-type', 'text/html');
-//     return res.end('<h1>Hello, Shot World World!</h1>');
+//     return res.end('<h1>Hello, Sham World World!</h1>');
 // });
+// app.get('/ramin', function (req, res) {
+//     res.header('Content-type', 'text/html');
+//     return res.end('<h1>Hello, Raminjan Kambiz your brother!</h1>');
+// });
+// app.get('/users/:userId/books/:bookId', function (req, res) {
+//   res.send(req.params)
+// })
+
+
 
 
--- routes/subscribers.js
+++ routes/subscribers.js
@@ -3,7 +3,9 @@ const router = express.Router()
 
 //Get All
 router.get("/", (req, resp) => {
-  resp.send("Hellow Subscriber World")  
+  //resp.send("Hellow Subscriber World")  
+  resp.header('Content-type', 'text/html');
+  return resp.end('<h1>Hello, Sham World World!</h1>');
 })
 //Get One
 router.get("/:id", (req, resp) => {
